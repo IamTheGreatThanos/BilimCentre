e3d90408840b756a61840afc689858954fb5e364
Dinmukhammed <iamthegreatthanos@gmail.com>
2020-07-20 15:51:14 +0600

    Initial Commit

---
+++ BilimCentre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BilimCentre
+//
+//  Created by THANOS on 7/20/20.
+//  Copyright © 2020 XCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
